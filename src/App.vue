ed216cd317838e025c80966a91ecda55db6d9556
ashleyryu <ryujw0108@gmail.com>
2019-07-02 14:22:25 +0900

    req1-1 | add footer

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,36 @@
       <router-view/>
       <!-- <HelloWorld/> -->
     </v-content>
+    <v-footer
+      height="auto"
+      color="primary lighten-1"
+    >
+      <v-layout
+        justify-center
+        row
+        wrap
+      >
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          flat
+          round
+        >
+          {{ link }}
+        </v-btn>
+        <v-flex
+          primary
+          lighten-2
+          py-3
+          text-xs-center
+          white--text
+          xs12
+        >
+          &copy;2019 — <strong>copyright 2019 Ashley Ryu All Rights Reserved.</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
@@ -34,6 +64,14 @@ export default {
     // HelloWorld
   },
   data () {
+    links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us'
+      ]
     return {
       //
     }
