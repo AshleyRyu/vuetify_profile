e2bd7f2052ecc97c26b3776923b89643bcfadb97
ashleyryu <ryujw0108@gmail.com>
2019-07-02 10:30:21 +0900

    init | npm, Vuetify install

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
     <v-content>
-      <router-view/>
+      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import store from './store'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-	name: 'App',
-	store,
-	data() {
-		return {
-			//
-		}
-	}
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
 }
 </script>
