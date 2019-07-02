f57696a12c81898cb1705ba7a703e6986b1e1456
ashleyryu <ryujw0108@gmail.com>
2019-07-02 14:38:03 +0900

    req1-4 | modify main img

--- src/components/ImgBanner.vue
+++ src/components/ImgBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-img :src="imgSrc"
+    <v-img src="https://source.unsplash.com/random"
            aspect-ratio="1.7">
       <v-layout align-center justify-center row fill-height>
         <v-flex text-xs-center>
